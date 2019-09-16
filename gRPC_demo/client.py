20c9151d674e56bf8cae4bc9664631336cb11ae4
zygoodest <zygoodest@126.com>
2019-09-16 15:12:04 +0800

    change s to stub

--- gRPC_demo/client.py
+++ gRPC_demo/client.py
@@ -1,24 +1,26 @@
-import sys
+#!/usr/bin/python3
 
+import sys
 import grpc
 
-from mytype import users_pb2_grpc as users_service
-from mytype import users_types_pb2 as users_messages
+sys.path.append("./interface")
+
+import test_pb2_grpc as s
+import test_pb2 as msg
 
 class Client(object):
     def __init__(self):
-        self.metadata = [('ip', '127.0.0.1')]
         self.grpc_host = "localhost:50051"          # grpc服务器地址
-        self.timeout = 10                           # 设定等待服务返回的超时时间(单位秒)
+        self.timeout = 10                           # 等待服务器的超时时间(秒)
         self.channel = None                         # 与grpc服务器的通道
-        self.s = None                               # 实例化的服务
-        self.response = None                        # 响应体
+        self.stub = None                            # 客户端的stub对象
+        self.respons = None                         # 响应体
 
         self.init_channel()
         self.init_server()
 
     def init_channel(self):
-        """与服务器建立通道(不安全通道)"""
+        """与服务器建立通道(不安全的通道)"""
         self.channel = grpc.insecure_channel(self.grpc_host)
         try:
             # 设定超时时间
@@ -29,26 +31,35 @@ class Client(object):
 
     def init_server(self):
         """实例化一个服务"""
-        self.s = users_service.UsersStub(self.channel)
+        self.stub = stub.TestStub(self.channel)
 
-    def CreateUser(self, username):
-        self.response = self.s.CreateUser(
-            users_messages.CreateUserRequest(username=username),
-            metadata = self.metadata
-        )
-        if self.response:
-            print("User created:", self.response.user.username)
 
-    def GetUsers(self):
-        request = users_messages.GetUsersRequest(
-            user=[users_messages.User(username="alexa", user_id=1),
-                  users_messages.User(username="christie", user_id=1)]
-        )
-        response = self.s.GetUsers(request, timeout=0.00001)
-        for i in response:
-            print(i)
+    def SayHello(self):
+        request = msg.MyRequest(name="zy")
+        response = self.stub.SayHello(request)
+        print(response)
 
 
 if __name__ == "__main__":
     c = Client()
-    c.CreateUser("zy")
+    c.SayHello()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
